d061829bb2dfff0051d34cba0892d15529e75b22
Lee David Painter <lee@ludup.com>
2014-10-15 15:44:08 +0100

    Added filename event attribute to FileOperationEvent.

--- hypersocket-fs/src/main/java/com/hypersocket/fs/events/FileOperationEvent.java
+++ hypersocket-fs/src/main/java/com/hypersocket/fs/events/FileOperationEvent.java
@@ -4,21 +4,25 @@ import com.hypersocket.fs.FileResource;
 import com.hypersocket.fs.FileResourceServiceImpl;
 import com.hypersocket.resource.ResourceSessionEvent;
 import com.hypersocket.session.Session;
+import com.hypersocket.util.FileUtils;
 
 public abstract class FileOperationEvent extends ResourceSessionEvent {
 
 	private static final long serialVersionUID = 2710591456847473963L;
 
 	public static final String ATTR_FILE_PATH = "attr.filePath";
+	public static final String ATTR_FILE_NAME = "attr.fileName";
 	public static final String ATTR_PROTOCOL = "attr.protocol";
 	
 	FileResource sourceResource;
-	String sourcePath;
-
+	String sourcePath;	
+	String filename;
+	
 	public FileOperationEvent(Object source, String resourceKey, boolean success,
 			Session session, FileResource sourceResource, String sourcePath, String protocol) {
 		super(source, resourceKey, success, session, sourceResource);
 		addAttribute(ATTR_FILE_PATH, sourcePath);
+		addAttribute(ATTR_FILE_NAME, filename = FileUtils.lastPathElement(sourcePath));
 		addAttribute(ATTR_PROTOCOL, protocol);
 	}
 	
@@ -26,6 +30,7 @@ public abstract class FileOperationEvent extends ResourceSessionEvent {
 			Session session, String sourceResource, String sourcePath, String protocol) {
 		super(source, resourceKey, sourceResource, e, session);
 		addAttribute(ATTR_FILE_PATH, sourcePath);
+		addAttribute(ATTR_FILE_NAME, filename = FileUtils.lastPathElement(sourcePath));
 		addAttribute(ATTR_PROTOCOL, protocol);
 	}
 	
@@ -39,6 +44,10 @@ public abstract class FileOperationEvent extends ResourceSessionEvent {
 
 	public String getSourcePath() {
 		return sourcePath;
+	}
+	
+	public String getFilename() {
+		return filename;
 	}
 	
 	
