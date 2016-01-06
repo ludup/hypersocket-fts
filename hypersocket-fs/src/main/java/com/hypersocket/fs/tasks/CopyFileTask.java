d2dc7edd565c3c6f53f625d6ceaa49602973e13d
ludup <lee@ludup.com>
2016-01-06 15:53:20 +0000

    Refactor of Task Result

--- hypersocket-fs/src/main/java/com/hypersocket/fs/tasks/CopyFileTask.java
+++ hypersocket-fs/src/main/java/com/hypersocket/fs/tasks/CopyFileTask.java
@@ -17,7 +17,7 @@ import com.hypersocket.realm.Realm;
 import com.hypersocket.tasks.AbstractTaskProvider;
 import com.hypersocket.tasks.Task;
 import com.hypersocket.tasks.TaskProviderService;
-import com.hypersocket.triggers.TaskResult;
+import com.hypersocket.triggers.AbstractTaskResult;
 import com.hypersocket.triggers.TriggerResourceService;
 import com.hypersocket.triggers.ValidationException;
 
@@ -79,7 +79,7 @@ public class CopyFileTask extends AbstractTaskProvider {
 	}
 
 	@Override
-	public TaskResult execute(Task task, Realm currentRealm, SystemEvent event)
+	public AbstractTaskResult execute(Task task, Realm currentRealm, SystemEvent event)
 			throws ValidationException {
 
 		String originPath = processTokenReplacements(repository.getValue(task, "origin.path"), event);
