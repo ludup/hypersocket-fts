219e1c0b03f82d835df204c3cea7a95d08061f21
gaurav <gaurav@g.com>
2016-07-07 15:42:13 +0530

    changes due to old ftp service removal

--- hypersocket-ftp/src/main/java/com/hypersocket/ftp/FTPService.java
+++ hypersocket-ftp/src/main/java/com/hypersocket/ftp/FTPService.java
@@ -3,4 +3,7 @@ package com.hypersocket.ftp;
 
 public interface FTPService {
 
+	public static final String AUTHENTICATION_SCHEME_NAME = "FTP";
+
+	public static final String AUTHENTICATION_SCHEME_RESOURCE_KEY = "ftp";
 }
