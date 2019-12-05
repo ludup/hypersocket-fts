c776134f89d375a5187759b09cbd9122be8fb30e
ludup <lee@ludup.com>
2019-12-05 15:01:08 +0000

    Add filename to file events.

--- hypersocket-fs/src/main/java/com/hypersocket/fs/tasks/FileOperationTaskResult.java
+++ hypersocket-fs/src/main/java/com/hypersocket/fs/tasks/FileOperationTaskResult.java
@@ -3,6 +3,7 @@ package com.hypersocket.fs.tasks;
 import org.apache.commons.lang3.ArrayUtils;
 
 import com.hypersocket.fs.tasks.copy.CopyFileTask;
+import com.hypersocket.json.utils.FileUtils;
 import com.hypersocket.realm.Realm;
 import com.hypersocket.tasks.Task;
 import com.hypersocket.triggers.AbstractTaskResult;
@@ -12,17 +13,21 @@ public class FileOperationTaskResult extends AbstractTaskResult {
 	private static final long serialVersionUID = -5267005980338783100L;
 
 	public static String ATTR_PATH = "attr.path";
+	public static String ATTR_FILE_NAME = "attr.fileName";
+	
 	
 	public FileOperationTaskResult(Object source, String resourceKey,
 			boolean success, Realm currentRealm, Task task, String path) {
 		super(source, resourceKey, success, currentRealm, task);
 		addAttribute(ATTR_PATH, path);
+		addAttribute(ATTR_FILE_NAME, FileUtils.lastPathElement(path));
 	}
 
 	public FileOperationTaskResult(Object source, String eventResourceKey,
 			Throwable e, Realm currentRealm, Task task, String path) {
 		super(source, eventResourceKey, e, currentRealm, task);
 		addAttribute(ATTR_PATH, path);
+		addAttribute(ATTR_FILE_NAME, FileUtils.lastPathElement(path));
 	}
 
 	@Override
