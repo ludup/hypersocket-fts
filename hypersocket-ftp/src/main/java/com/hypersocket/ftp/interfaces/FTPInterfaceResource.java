833aebbcbb54cf451616a6c53e849d1d6b99b373
Brett Smith <brett@logonbox.com>
2020-04-24 14:30:25 +0100

    More flaws found in realm deletion. Hibernate was re-adding different foreign keys (non cascading delete) on the next start up, because the names differed from those that would be autogenerated. MySQL will accept this schema change so you end up with multiple foreign keys with different actions.
    So after a few false starts, I have settled on the folowing strategu.
    
      * UpdateService now supports ".pre.sql" scripts. These are run BEFORE Hibnerate is initialized
      * core_2_2_1.pre.sql drops ALL foreign keys before Hibernate starts
      * Hibernate starts up and adds all the correctly cascaded keys
      * The special case is the audit table which drops it's own keys and recreates.
    
    All of the existing SQL scripts have been removed, this is now (nearly) entirely done through the annotations and class structure.
    
    One less than pleasant part of this was the need to move the Realm property in RealmResource, and RemoteUser/RemoteGroup principal property in RemoteUserAttributes/RemoteGroupAttributes down into the concrete implementations.
    This is because this doesn't seem to work with these tables, and the @AttributeOverride annotation doesnt work either. Both of these seem to be due to known bugs in Hibernate.

--- hypersocket-ftp/src/main/java/com/hypersocket/ftp/interfaces/FTPInterfaceResource.java
+++ hypersocket-ftp/src/main/java/com/hypersocket/ftp/interfaces/FTPInterfaceResource.java
@@ -2,32 +2,54 @@ package com.hypersocket.ftp.interfaces;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import com.hypersocket.certificates.CertificateResource;
+import com.hypersocket.realm.Realm;
 import com.hypersocket.server.interfaces.InterfaceResource;
 
 @Entity
-@Table(name="ftp_interfaces")
+@Table(name = "ftp_interfaces")
 public class FTPInterfaceResource extends InterfaceResource {
 
 	private static final long serialVersionUID = -7196348246139661166L;
-	
-	@Column(name="protocol")
+
+	@Column(name = "protocol")
 	private FTPProtocol ftpProtocol;
-	
-	@Column(name="passive_ports")
+
+	@Column(name = "passive_ports")
 	private String ftpPassivePorts;
-	
-	@Column(name="passive_external_address")
+
+	@Column(name = "passive_external_address")
 	private String ftpPassiveExternalAddress;
-	
-	@Column(name="idle_timeout")
+
+	@Column(name = "idle_timeout")
 	private Integer ftpIdleTimeout;
-	
+
 	@OneToOne
 	private CertificateResource ftpCertificate;
+
+	@ManyToOne
+	@JoinColumn(name = "realm_id", foreignKey = @ForeignKey(name = "ftp_interfaces_cascade_1"))
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	protected Realm realm;
+
+	@Override
+	protected Realm doGetRealm() {
+		return realm;
+	}
+
+	@Override
+	public void setRealm(Realm realm) {
+		this.realm = realm;
+	}
 
 	public FTPProtocol getFtpProtocol() {
 		return ftpProtocol;
